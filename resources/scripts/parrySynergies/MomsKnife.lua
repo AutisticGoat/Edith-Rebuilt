42bb55630d9121976c52690efbd44a9d1f32aa43
AutisticGoat <smhkotry@gmail.com>
2025-12-19 05:00:19 -0700

    Temporarily removed Tainted Edith's Mom's Knife parry synergy

--- resources/scripts/parrySynergies/MomsKnife.lua
+++ resources/scripts/parrySynergies/MomsKnife.lua
@@ -2,26 +2,28 @@ local mod = EdithRebuilt
 local callbacks = mod.Enums.Callbacks
 local data = mod.CustomDataWrapper.getData
 
----@param player EntityPlayer
-mod:AddCallback(callbacks.OFFENSIVE_STOMP, function(_, player)
-	if not player:HasCollectible(CollectibleType.COLLECTIBLE_MOMS_KNIFE) then return end		
+-- I need to think about something different
 
-	local knifeEntities = player:HasCollectible(CollectibleType.COLLECTIBLE_BIRTHRIGHT) and 8 or 4
-	local degrees = 360/knifeEntities
-	local knife
+-- ---@param player EntityPlayer
+-- mod:AddCallback(callbacks.OFFENSIVE_STOMP, function(_, player)
+-- 	if not player:HasCollectible(CollectibleType.COLLECTIBLE_MOMS_KNIFE) then return end		
 
-	for i = 1, knifeEntities do
-		knife = player:FireKnife(player, degrees * i, true, 0, 0)
-		knife:Shoot(1, player.TearRange / 3)
-		data(knife).ParryKnife = true			
-	end
-end)
+-- 	local knifeEntities = player:HasCollectible(CollectibleType.COLLECTIBLE_BIRTHRIGHT) and 8 or 4
+-- 	local degrees = 360/knifeEntities
+-- 	local knife
 
----@param knife EntityKnife
-function mod:RemoveKnife(knife)	
-	if not data(knife).ParryKnife then return end
-	if knife:IsFlying() then return end 
+-- 	for i = 1, knifeEntities do
+-- 		knife = player:FireKnife(player, degrees * i, true, 0, 0)
+-- 		knife:Shoot(1, player.TearRange / 3)
+-- 		data(knife).ParryKnife = true			
+-- 	end
+-- end)
 
-	knife:Remove()
-end
-mod:AddCallback(ModCallbacks.MC_POST_KNIFE_UPDATE, mod.RemoveKnife)
+-- ---@param knife EntityKnife
+-- function mod:RemoveKnife(knife)	
+-- 	if not data(knife).ParryKnife then return end
+-- 	if knife:IsFlying() then return end 
+
+-- 	knife:Remove()
+-- end
+-- mod:AddCallback(ModCallbacks.MC_POST_KNIFE_UPDATE, mod.RemoveKnife)
