95a5a824c3a23d0e1150be79f2412567da5eaffa
AutisticGoat <smhkotry@gmail.com>
2025-12-22 01:05:27 -0700

    Added The Future support

---
+++ resources/scripts/compat/TheFuture.lua
@@ -0,0 +1,10 @@
+---@diagnostic disable: undefined-global
+if not TheFuture then return end
+
+TheFuture.ModdedCharacterDialogue["Edith​​​"] = {
+        "Hmm...",
+        "Wait, I think I have seen you before...",
+        "Actually... I think I have seen you like 4 or 5 times before",
+        "...",
+        "...who are you?"
+}
