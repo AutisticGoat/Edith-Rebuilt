3ce6f00a450d1dbf58ca2d4372c0da6a1e43a39e
AutisticGoat <smhkotry@gmail.com>
2025-12-21 23:43:49 -0700

    Fixed Salt rocks not triggering salted effect to enemies

--- resources/scripts/collectibles/consumables/SaltRocks.lua
+++ resources/scripts/collectibles/consumables/SaltRocks.lua
@@ -2,12 +2,18 @@ local mod = EdithRebuilt
 local enums = mod.Enums
 local card = enums.Card
 local Helpers = mod.Modules.HELPERS
+local StatusEffects = mod.Modules.STATUS_EFFECTS
 local SaltRocks = {}
+
+--[[
+    On use:
+    - Permanently Applies salted status effect to all enemies in room
+]]
 
 ---@param player EntityPlayer
 function SaltRocks:OnSaltRockUse(_, player)
     for _, enemy in pairs(Helpers.GetEnemies()) do
-        mod.SetSalted(enemy, -1, player)
+        StatusEffects.SetStatusEffect("Salted", enemy, -1, player)
     end
 end
 mod:AddCallback(ModCallbacks.MC_USE_CARD, SaltRocks.OnSaltRockUse, card.CARD_SALT_ROCKS)
