75e30c83348fcdbd8d7bf25ddc42c689574bc966
AutisticGoat <smhkotry@gmail.com>
2026-01-22 02:02:07 -0700

    Tweaked Sulfuric Fire
    - Now uses an special function to spawn firejets
    - Added a damage mult when using it with Judas while having birthright (x1.5)
    - Added a damage mult when using it while having Car Battery (x1.25)
    - Added a screen shake on use

--- resources/scripts/collectibles/items/Edith/SulfuricFire.lua
+++ resources/scripts/collectibles/items/Edith/SulfuricFire.lua
@@ -1,28 +1,35 @@
 local mod = EdithRebuilt
 local enums = mod.Enums
 local items = enums.CollectibleType
+local Helpers = mod.Modules.HELPERS
+local Player = mod.Modules.PLAYER
 local SulfuricFire = {}
 
 ---@param player EntityPlayer
----@return boolean
-function SulfuricFire:UseSulfuricFire(_, _, player)
-	local Flame
-	for _, enemy in pairs(Isaac.FindInRadius(player.Position, 80, EntityPartition.ENEMY)) do
-		Flame = Isaac.Spawn(
-			EntityType.ENTITY_EFFECT,
-			EffectVariant.FIRE_JET,
-			0,
-			enemy.Position,
-			Vector.Zero,
-			player
-		)		
-		Flame.CollisionDamage = player.Damage + (enemy.MaxHitPoints * 0.175)
-		enemy:AddBrimstoneMark(EntityRef(player), 150)
+---@param flag UseFlag
+---@return boolean?
+function SulfuricFire:UseSulfuricFire(_, _, player, flag)
+	if flag & UseFlag.USE_CARBATTERY == UseFlag.USE_CARBATTERY then return end
+	local ref = EntityRef(player)
+	local IsJudasWithBirthright = Player.IsJudasWithBirthright(player)
+	local HasCarBattery = player:HasCollectible(CollectibleType.COLLECTIBLE_CAR_BATTERY) 
+	local radius = IsJudasWithBirthright and 150 or 100 
+	local JudasMult = IsJudasWithBirthright and 1.5 or 1
+	local CarBatteryMult = HasCarBattery and 1.25 or 1
+
+	for _, enemy in pairs(Isaac.FindInRadius(player.Position, radius, EntityPartition.ENEMY)) do
+		local damage = player.Damage + (enemy.MaxHitPoints * 0.175)
+		local Flame = Helpers.SpawnFireJet(enemy.Position, (damage * JudasMult) * CarBatteryMult)
+		
+
+		enemy:AddBrimstoneMark(ref, 150)
 
 		if enemy.HitPoints > Flame.CollisionDamage then goto Continue end
 		player:FireBrimstoneBall(enemy.Position, Vector.Zero)
 		::Continue::
 	end
+
+	enums.Utils.Game:ShakeScreen(12)
 	return true
 end
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, SulfuricFire.UseSulfuricFire, items.COLLECTIBLE_SULFURIC_FIRE)
