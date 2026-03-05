a5d234cfb92176c91d62164e0ba5dbf3cc322b8b
AutisticGoat <83523192+AutisticGoat@users.noreply.github.com>
2026-03-05 16:50:23 -0700

    Edith's Hood Tweaks
    - Now its Damage multiplier is added through XML
    - Increased stomp damage (player Damage mult, 1.25 > 1.5)
    - Increased stomp knockback (8 > 15)
    - General code cleanup

--- resources/scripts/collectibles/items/Edith/EdithsHood.lua
+++ resources/scripts/collectibles/items/Edith/EdithsHood.lua
@@ -2,11 +2,11 @@ local mod = EdithRebuilt
 local enums = mod.Enums
 local items = enums.CollectibleType
 local saltTypes = enums.SaltTypes
-local Player = mod.Modules.PLAYER
 local tables = enums.Tables
 local jumpTags = tables.JumpTags
 local jumpParams = tables.JumpParams
 local modules = mod.Modules
+local Player = modules.PLAYER
 local Math = modules.MATHS
 local helpers = modules.HELPERS
 local RNGMod = modules.RNG
@@ -15,10 +15,11 @@ local Land = modules.LAND
 local Creeps = modules.CREEPS
 local sfx = enums.Utils.SFX
 local data = mod.DataHolder.GetEntityData
-local EdithsHood = {}
+local maxCreep = 10
+local saltDegrees = 360 / maxCreep
 
 ---@param tear EntityTear
-function EdithsHood:ShootSaltTears(tear)
+mod:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, function(_, tear)
 	local player = helpers.GetPlayerFromTear(tear)
 
 	if not player then return end
@@ -26,20 +27,10 @@ function EdithsHood:ShootSaltTears(tear)
 	if not player:HasCollectible(items.COLLECTIBLE_EDITHS_HOOD) then return end
 
 	helpers.ForceSaltTear(tear, false)
-end
-mod:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, EdithsHood.ShootSaltTears)
+end)
 
 ---@param player EntityPlayer
-function EdithsHood:Stats(player)
-	if not player:HasCollectible(items.COLLECTIBLE_EDITHS_HOOD) then return end
-	player.Damage = player.Damage * 1.35
-end
-mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, EdithsHood.Stats, CacheFlag.CACHE_DAMAGE)
-
-local maxCreep = 10
-local saltDegrees = 360 / maxCreep
-
-function EdithsHood:JumpCooldown(player)
+mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, function(_, player)
 	if not player:HasCollectible(items.COLLECTIBLE_EDITHS_HOOD) then return end
 	if JumpLib:GetData(player).Jumping then return end
 
@@ -52,11 +43,11 @@ function EdithsHood:JumpCooldown(player)
 	local soundTab = tables.CooldownSounds[EdithSave.JumpCooldownSound or 1]
 	local pitch = soundTab.Pitch == 1.2 and (soundTab.Pitch * RNGMod.RandomFloat(player:GetDropRNG(), 1, 1.1)) or soundTab.Pitch
 	sfx:Play(soundTab.SoundID, 2, 0, false, pitch)
-	Player.SetColorCooldown(player, 0.6, 5)
-end
-mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, EdithsHood.JumpCooldown)
+	Player.SetColorCooldown(player, 0.6, 5)	
+end)
 
-function EdithsHood:TriggerJump(player)
+---@param player EntityPlayer
+mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, function(_, player)
 	local playerData = data(player)
 
 	if Player.IsAnyEdith(player) then return end
@@ -67,20 +58,22 @@ function EdithsHood:TriggerJump(player)
 	playerData.HoodJumpTimer = 60
 
 	Edith.InitEdithJump(player, jumpTags.EdithsHoodJump)
-end
-mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, EdithsHood.TriggerJump)
+end)
 
-function EdithsHood:OnHoodJumpLand(player)
+---@param player EntityPlayer
+mod:AddCallback(JumpLib.Callbacks.ENTITY_LAND, function (_, player)
 	if Player.IsAnyEdith(player) then return end
 
 	local params = Edith.GetJumpStompParams(player)
+	local playerData = data(player)
 
-	params.Damage = (player.Damage * 1.25) * 3
+	params.Damage = (player.Damage * 1.5) * 3
 	params.Radius = 40
-	params.Knockback = 8
+	params.Knockback = 15
 
-	data(player).HoodLand = true
+	playerData.HoodLand = true
 	Land.LandFeedbackManager(player, Land.GetLandSoundTable(false), Color.Default, false)
+	playerData.HoodLand = false
 	Land.EdithStomp(player, params, false)
 	Land.TriggerLandenemyJump(player, params.StompedEntities, params.Knockback, 10, 1.8)
 
@@ -88,14 +81,12 @@ function EdithsHood:OnHoodJumpLand(player)
 		Creeps.SpawnSaltCreep(player, player.Position + Vector(0, 30):Rotated(saltDegrees*i), 0.1, 5, 1, 3, saltTypes.EDITHS_HOOD)
 	end
 	player:SetMinDamageCooldown(30)
-	data(player).HoodLand = false
-end
-mod:AddCallback(JumpLib.Callbacks.ENTITY_LAND, EdithsHood.OnHoodJumpLand, jumpParams.EdithsHoodJump)
+end, jumpParams.EdithsHoodJump)
 
 ---@param npc EntityNPC
 ---@param source EntityRef
-function EdithsHood:OnSaltedDeath(npc, source)
-    local saltedType = data(npc).SaltType ---@cast saltedType SaltTypes
+mod:AddCallback(PRE_NPC_KILL.ID, function(_, npc, source)
+	local saltedType = data(npc).SaltType ---@cast saltedType SaltTypes
     local player = helpers.GetPlayerFromRef(source)
 
     if not player then return end
@@ -106,8 +97,6 @@ function EdithsHood:OnSaltedDeath(npc, source)
 
 	for _ = 1, randomTears do
 		local tear = player:FireTear(npc.Position, rng:RandomVector():Resized(player.ShotSpeed * 10), false, false, false, player, 1.2)
-
 		tear:AddTearFlags(player.TearFlags)
 	end
-end 
-mod:AddCallback(PRE_NPC_KILL.ID, EdithsHood.OnSaltedDeath)
+end)
