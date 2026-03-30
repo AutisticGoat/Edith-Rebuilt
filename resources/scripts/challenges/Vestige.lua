7dcd60ed760d305d2f948cd36ec1f3ee8ddefd98
AutisticGoat <83523192+AutisticGoat@users.noreply.github.com>
2026-03-29 19:50:32 -0700

    Remade Vestige challenge script from scratch

--- resources/scripts/challenges/Vestige.lua
+++ resources/scripts/challenges/Vestige.lua
@@ -6,25 +6,34 @@ local TargetArrow = modules.TARGET_ARROW
 local Helpers = modules.HELPERS
 local EdithMod = modules.EDITH
 local Player = modules.PLAYER
-local Vestige = {}
+local Jump = modules.JUMP
 
 ---@param player EntityPlayer
-function Vestige:EdithJumpHandler(player)
-    if not Helpers.IsVestigeChallenge() then return end
-	if not Player.IsEdith(player, false) then return end
+---@return boolean
+local function IsEdithAndVestige(player)
+	return Player.IsEdith(player, false) and Helpers.IsVestigeChallenge()
+end
+
+---@param player EntityPlayer
+mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, function(_, player)
+	if not IsEdithAndVestige(player) then return end
+	player.TearRange = Player.rangeUp(player.TearRange, 1.75)
+end, CacheFlag.CACHE_RANGE)
+
+---@param player EntityPlayer
+mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, function(_, player)
+	if not IsEdithAndVestige(player) then return end
 
 	local playerData = data(player)
-	if player:IsDead() then TargetArrow.RemoveEdithTarget(player); playerData.isJumping = false return end
+	if player:IsDead() then 
+		TargetArrow.RemoveEdithTarget(player) 
+		return
+	end
 
 	local isKeyStompTriggered = Helpers.IsKeyStompTriggered(player)
 	local jumpData = JumpLib:GetData(player)
 	local isJumping = jumpData.Jumping 
-	local target = TargetArrow.GetEdithTarget(player)
 	local sprite = player:GetSprite()
-	local jumpInternalData = JumpLib.Internal:GetData(player)
-
-	playerData.isJumping = playerData.isJumping or false
-	playerData.ExtraJumps = playerData.ExtraJumps or 0
 
     if isKeyStompTriggered and not isJumping and not sprite:IsPlaying("BigJumpUp") and not sprite:IsPlaying("BigJumpFinish") then
         player:PlayExtraAnimation("BigJumpUp")
@@ -33,21 +42,24 @@ function Vestige:EdithJumpHandler(player)
     if sprite:IsEventTriggered("StartJump") and not isJumping then
         EdithMod.InitEdithJump(player, JumpTags.EdithJump, true)
     end
+end)
 
-	if jumpInternalData.UpdateFrame and jumpInternalData.UpdateFrame > 6 then
-		EdithMod.EdithDash(player, TargetArrow.GetEdithTargetDirection(player), TargetArrow.GetEdithTargetDistance(player), 50)
+---@param player EntityPlayer
+mod:AddCallback(JumpLib.Callbacks.ENTITY_UPDATE_60, function (_, player)
+	if not IsEdithAndVestige(player) then return end
+
+	local target = TargetArrow.GetEdithTarget(player)
+	local IsFalling = JumpLib:IsFalling(player)
+	local targetDistance = TargetArrow.GetEdithTargetDistance(player)
+
+	if not target then return end
+
+	if Jump.GetJumpFrame(player) > 6 then
+		EdithMod.EdithDash(player, TargetArrow.GetEdithTargetDirection(player), targetDistance, 50)
 	end
 
-	if target and (JumpLib:IsFalling(player) or (isJumping and player.Position:Distance(target.Position) <= 5))  then
+	if IsFalling or targetDistance <= 5 then
 		player.Velocity = Vector.Zero
 		player.Position = target.Position
 	end
-end
-mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, Vestige.EdithJumpHandler)
-
----@param player EntityPlayer
-mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, function(_, player)
-	if not Player.IsEdith(player, false) then return end
-	if not Helpers.IsVestigeChallenge() then return end
-	player.TearRange = Player.rangeUp(player.TearRange, 1.75)
-end, CacheFlag.CACHE_RANGE)
+end, EdithRebuilt.Enums.Tables.JumpParams.EdithJump)
