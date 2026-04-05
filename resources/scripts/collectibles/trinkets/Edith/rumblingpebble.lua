f8a67036e47752ec39cfae9303891b4ff58575ef
AutisticGoat <83523192+AutisticGoat@users.noreply.github.com>
2026-04-05 05:03:20 -0700

    Update rumblingpebble.lua
    - Now Rumbling Pebble detects the player that destroyed a rock
    - Now it can make rock tears spawn from bomb-incrusted rocks

--- resources/scripts/collectibles/trinkets/Edith/rumblingpebble.lua
+++ resources/scripts/collectibles/trinkets/Edith/rumblingpebble.lua
@@ -9,64 +9,65 @@ local Helpers = modules.HELPERS
 local Player = modules.PLAYER
 local RumblingPebble = {}
 
-function RumblingPebble:DestroyRockWithPebble(rock)	
-	for _, player in pairs(PlayerManager.GetPlayers()) do
-		if not player:HasTrinket(trinket.TRINKET_RUMBLING_PEBBLE) then goto continue end
-		local rng = player:GetTrinketRNG(trinket.TRINKET_RUMBLING_PEBBLE)
-		local trinketMult = player:GetTrinketMultiplier(trinket.TRINKET_RUMBLING_PEBBLE)
-		local totalrocks = rng:RandomInt(2 + trinketMult, 4 + trinketMult)
-		local shootDegree = 360 / totalrocks
-		local rangemult = Player.GetPlayerRange(player) / 6.5
-		local FallSpeedVar
+---@param rock GridEntityRock
+---@param type GridEntityType
+---@param source EntityRef
+mod:AddCallback(ModCallbacks.MC_POST_GRID_ROCK_DESTROY, function(_, rock, type, _, source)
+	if source.Type == 0 then return end
 
-		for i = 1, totalrocks do
-			local rockTear = Isaac.Spawn(
-				EntityType.ENTITY_TEAR,
-				TearVariant.ROCK,
-				0,
-				rock.Position,
-				Vector.One:Rotated((shootDegree + rng:RandomInt(-10, 10)) * i):Resized(10),
-				player
-			):ToTear()
-			
-			if not rockTear then return end
-			
-			FallSpeedVar = ModRNG.RandomFloat(rng, 0.8, 1.2)
+	local player = Helpers.GetPlayerFromRef(source)
 
-			rockTear:AddTearFlags(player.TearFlags | TearFlags.TEAR_ROCK)
-			rockTear.FallingAcceleration = ModRNG.RandomFloat(rng, 1.5, 2)
-			rockTear.FallingSpeed = (-10 * rangemult) * FallSpeedVar
-			rockTear.Height = baseHeight * rangemult
-			rockTear.Scale = FallSpeedVar
-			rockTear.CollisionDamage = 5.25 * rockTear.Scale
-			
-			data(rockTear).IsPebbleTear = true
-		end
-		::continue::
+	if not player then return end
+	if not player:HasTrinket(trinket.TRINKET_RUMBLING_PEBBLE) then return end
+	if type == GridEntityType.GRID_ROCK_BOMB then return end
+
+	local rng = player:GetTrinketRNG(trinket.TRINKET_RUMBLING_PEBBLE)
+	local trinketMult = player:GetTrinketMultiplier(trinket.TRINKET_RUMBLING_PEBBLE)
+	local totalrocks = rng:RandomInt(2 + trinketMult, 4 + trinketMult)
+	local shootDegree = 360 / totalrocks
+	local rangemult = Player.GetPlayerRange(player) / 6.5
+
+	for i = 1, totalrocks do
+		local rockTear = player:FireTear(
+			rock.Position, 
+			Vector.One:Rotated((shootDegree + rng:RandomInt(-10, 10) * i)):Resized(10)
+		)
+
+		local FallSpeedVar = ModRNG.RandomFloat(rng, 0.8, 1.2)
+
+		rockTear:AddTearFlags(player.TearFlags | TearFlags.TEAR_ROCK)
+		rockTear.FallingAcceleration = ModRNG.RandomFloat(rng, 1.5, 2)
+		rockTear.FallingSpeed = (-10 * rangemult) * FallSpeedVar
+		rockTear.Height = baseHeight * rangemult
+		rockTear.Scale = FallSpeedVar
+		rockTear.CollisionDamage = 5.25 * rockTear.Scale
+
+		data(rockTear).IsPebbleTear = true
 	end
-end
-mod:AddCallback(ModCallbacks.MC_POST_GRID_ROCK_DESTROY, RumblingPebble.DestroyRockWithPebble)
+end)
 
 ---@param tear EntityTear
-function RumblingPebble:RandomRockTear(tear)
+mod:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, function(_, tear)
 	local player = Helpers.GetPlayerFromTear(tear)
 
 	if not player then return end 
 	if not player:HasTrinket(trinket.TRINKET_RUMBLING_PEBBLE) then return end
+	
 	local rng = player:GetTrinketRNG(trinket.TRINKET_RUMBLING_PEBBLE)
 
-	if not ModRNG.RandomBoolean(rng, 0.3) then return end
+	if not ModRNG.RandomBoolean(rng) then return end
 	tear.CollisionDamage = tear.CollisionDamage * ModRNG.RandomFloat(rng, 0.5, 2)
 	tear:AddTearFlags(TearFlags.TEAR_ROCK)
 	tear:ChangeVariant(TearVariant.ROCK)
 	data(tear).IsPebbleTear = true
-end
-mod:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, RumblingPebble.RandomRockTear)
+end)
 
+---@param tear EntityTear
+---@param grid GridEntity
 function RumblingPebble:ShootTear(tear, _, grid)
 	if not grid:ToRock() then return end
 	if not data(tear).IsPebbleTear then return end
 		
-	grid:Destroy()
+	grid:DestroyWithSource(false, EntityRef(Helpers.GetPlayerFromTear(tear)))
 end
 mod:AddCallback(ModCallbacks.MC_PRE_TEAR_GRID_COLLISION, RumblingPebble.ShootTear)
