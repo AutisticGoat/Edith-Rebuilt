a5aecad2ba40b7f32901a4fca13c90577983da41
AutisticGoat <83523192+AutisticGoat@users.noreply.github.com>
2026-04-05 05:01:33 -0700

    General Grudge challenge script refactor

--- resources/scripts/challenges/Grudge.lua
+++ resources/scripts/challenges/Grudge.lua
@@ -7,32 +7,34 @@ local TEdithMod = modules.TEDITH
 local Player = modules.PLAYER
 local Grudge = {}
 
----@param player EntityPlayer
----@param collider Entity
-function Grudge:OnTaintedEdithGrudgeCollision(player, collider)
-    if not Player.IsEdith(player, true) then return end
-    if not Helpers.IsGrudgeChallenge() then return end
-
+local function HandleFireplaceCollision(collider)
     if collider.Type == EntityType.ENTITY_FIREPLACE and collider.Variant ~= 4 then
         collider:Die()
     end
+end
 
+local function HandleGrudgeDashDamage(player, collider, hopParams)
+    if not hopParams.GrudgeDash then return end
     if not Helpers.IsEnemy(collider) then return end
 
-    local HopParams = TEdithMod.GetHopParryParams(player)
-
-    if not HopParams.GrudgeDash then return end
-
-    local Aceleration = player.Velocity:Length()
-    local Damage = (
+    local speed = player.Velocity:Length()
+    local damage = (
         (player.Damage / 2) +
-        (player.MoveSpeed) / 3 + 
-        (Aceleration / 4) * ((TEdithMod.GetHopDashCharge(player, false, true) / 200) / 2)
+        (player.MoveSpeed) / 3 +
+        (speed / 4) * ((TEdithMod.GetHopDashCharge(player, false, true) / 200) / 2)
     )
 
     sfx:Play(SoundEffect.SOUND_MEATY_DEATHS)
-
     collider.Velocity = collider.Velocity + player.Velocity
-    collider:TakeDamage(Damage, DamageFlag.DAMAGE_CRUSH, EntityRef(player), 0)
+    collider:TakeDamage(damage, DamageFlag.DAMAGE_CRUSH, EntityRef(player), 0)
 end
-mod:AddCallback(ModCallbacks.MC_PRE_PLAYER_COLLISION, Grudge.OnTaintedEdithGrudgeCollision)
+
+---@param player EntityPlayer
+---@param collider Entity
+mod:AddCallback(ModCallbacks.MC_PRE_PLAYER_COLLISION, function(_, player, collider)
+    if not Player.IsEdith(player, true) then return end
+    if not Helpers.IsGrudgeChallenge() then return end
+
+    HandleFireplaceCollision(collider)
+    HandleGrudgeDashDamage(player, collider, TEdithMod.GetHopParryParams(player))
+end)
